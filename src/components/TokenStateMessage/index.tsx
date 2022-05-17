a249a22bb0f4e819f298c6394647c48da65538a3
chenh <1433255094@qq.com>
2022-05-17 14:27:15 +0800

    feat: use link with base url

--- src/components/TokenStateMessage/index.tsx
+++ src/components/TokenStateMessage/index.tsx
@@ -1,13 +1,18 @@
 import { Message } from '@alifd/next';
 import { useLocalStorageState } from 'ahooks';
 import React from 'react';
+import {useBaseUrlUtils} from '@docusaurus/useBaseUrl';
 
 function TokenStateMessage() {
   const [token] = useLocalStorageState<string>('se-token', { defaultValue: '' });
+  const { withBaseUrl } = useBaseUrlUtils();
 
   return (
     !token ? (
-      <Message type="error" title={<div>Please <a href={'get-token'}>get token and save</a> first</div>} />
+      <Message
+        type="error"
+        title={<div>Please <a href={withBaseUrl('/docs/get-token')}>get token and save</a> first</div>}
+      />
     ) : null
   );
 }
